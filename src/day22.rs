18c1cfccbeb1d6dc9cf721df266fc06dff9ae79c
Luminiscental <luminiscental01@gmail.com>
2023-12-22 13:13:26 +0000

    Clean up day 22

--- src/day22.rs
+++ src/day22.rs
@@ -7,23 +7,72 @@ use itertools::iproduct;
 
 use crate::day::Day;
 
-fn brick_range(b: ([usize; 3], [usize; 3])) -> impl Iterator<Item = [usize; 3]> {
+#[derive(Default)]
+pub struct Support {
+    up: HashMap<usize, HashSet<usize>>,
+    down: HashMap<usize, HashSet<usize>>,
+}
+
+fn range(b: ([usize; 3], [usize; 3])) -> impl Iterator<Item = [usize; 3]> {
     iproduct!(b.0[0]..=b.1[0], b.0[1]..=b.1[1], b.0[2]..=b.1[2]).map(|(x, y, z)| [x, y, z])
 }
 
-fn count_dropped(
-    brick: usize,
-    resters: &HashMap<usize, HashSet<usize>>,
-    supporters: &HashMap<usize, HashSet<usize>>,
-) -> usize {
-    let mut dropped = HashSet::<usize>::new();
-    let mut queue = VecDeque::new();
+fn lower(b: ([usize; 3], [usize; 3]), d: usize) -> ([usize; 3], [usize; 3]) {
+    ([b.0[0], b.0[1], b.0[2] - d], [b.1[0], b.1[1], b.1[2] - d])
+}
+
+fn settle_bricks(bricks: &mut [([usize; 3], [usize; 3])]) -> HashMap<[usize; 3], usize> {
+    let mut space = bricks
+        .iter()
+        .enumerate()
+        .flat_map(|(i, &b)| range(b).map(move |p| (p, i)))
+        .collect::<HashMap<_, _>>();
+    let mut moved = true;
+    while moved {
+        moved = false;
+        for (i, b) in bricks.iter_mut().enumerate() {
+            let mut scan = *b;
+            scan.1[2] = scan.0[2];
+            if let Some(j) = (1..b.0[2])
+                .take_while(|&j| range(lower(scan, j)).all(|p| !space.contains_key(&p)))
+                .last()
+            {
+                for p in range(*b) {
+                    space.remove(&p);
+                }
+                *b = lower(*b, j);
+                space.extend(range(*b).map(|p| (p, i)));
+                moved = true;
+            }
+        }
+    }
+    space
+}
+
+fn get_support(bricks: &[([usize; 3], [usize; 3])], space: &HashMap<[usize; 3], usize>) -> Support {
+    let mut support = Support::default();
+    for (i, &b) in bricks.iter().enumerate() {
+        let mut scan = lower(b, 1);
+        scan.1[2] = scan.0[2];
+        for p in range(scan) {
+            if let Some(j) = space.get(&p).copied() {
+                support.up.entry(j).or_default().insert(i);
+                support.down.entry(i).or_default().insert(j);
+            }
+        }
+    }
+    support
+}
+
+fn count_dropped(brick: usize, support: &Support) -> usize {
+    let (mut dropped, mut queue) = (HashSet::new(), VecDeque::new());
     queue.push_back(brick);
     while let Some(i) = queue.pop_front() {
         dropped.insert(i);
-        if let Some(rs) = resters.get(&i) {
+        if let Some(rs) = support.up.get(&i) {
             queue.extend(rs.iter().filter(|r| {
-                supporters
+                support
+                    .down
                     .get(r)
                     .unwrap()
                     .iter()
@@ -40,11 +89,7 @@ impl<'a> Day<'a> for Day22 {
     const DAY: usize = 22;
 
     type Input = Vec<([usize; 3], [usize; 3])>;
-    type ProcessedInput = (
-        usize,
-        HashMap<usize, HashSet<usize>>,
-        HashMap<usize, HashSet<usize>>,
-    );
+    type ProcessedInput = Vec<usize>;
 
     fn parse(input: &'a str) -> Self::Input {
         input
@@ -60,69 +105,18 @@ impl<'a> Day<'a> for Day22 {
             .collect()
     }
 
-    fn solve_part1(input: Self::Input) -> (Self::ProcessedInput, String) {
-        let mut bricks = input.clone();
-        let mut space = bricks
-            .iter()
-            .enumerate()
-            .flat_map(|(i, &b)| brick_range(b).map(move |p| (p, i)))
-            .collect::<HashMap<_, _>>();
-        let lower = |mut b: ([usize; 3], [usize; 3]), d: usize| {
-            b.0[2] -= d;
-            b.1[2] -= d;
-            b
-        };
-        let mut moved = true;
-        while moved {
-            moved = false;
-            for (i, b) in bricks.iter_mut().enumerate() {
-                let mut scan = *b;
-                scan.1[2] = scan.0[2];
-                if let Some(j) = (1..b.0[2])
-                    .take_while(|&j| brick_range(lower(scan, j)).all(|p| !space.contains_key(&p)))
-                    .last()
-                {
-                    for p in brick_range(*b) {
-                        space.remove(&p);
-                    }
-                    *b = lower(*b, j);
-                    for p in brick_range(*b) {
-                        space.insert(p, i);
-                    }
-                    moved = true;
-                }
-            }
-        }
-        let mut resters = HashMap::<usize, HashSet<usize>>::new();
-        let mut supporters = HashMap::<usize, HashSet<usize>>::new();
-        for (i, &b) in bricks.iter().enumerate() {
-            let vert = b.0[2] != b.1[2];
-            let mut scan = lower(b, 1);
-            if vert {
-                scan = (scan.0, scan.0);
-            }
-            for p in brick_range(scan) {
-                if let Some(j) = space.get(&p).copied() {
-                    resters.entry(j).or_default().insert(i);
-                    supporters.entry(i).or_default().insert(j);
-                }
-            }
-        }
-        let ans = (0..bricks.len())
-            .filter(|i| {
-                resters.get(i).map_or(true, |r| {
-                    r.iter().all(|j| supporters.get(j).unwrap().len() > 1)
-                })
-            })
-            .count();
-        ((bricks.len(), resters, supporters), ans.to_string())
+    fn solve_part1(mut bricks: Self::Input) -> (Self::ProcessedInput, String) {
+        let space = settle_bricks(&mut bricks);
+        let support = get_support(&bricks, &space);
+        let counts = (0..bricks.len())
+            .map(|i| count_dropped(i, &support))
+            .collect::<Vec<_>>();
+        let ans = counts.iter().filter(|n| **n == 0).count();
+        (counts, ans.to_string())
     }
 
-    fn solve_part2((bricks, resters, supporters): Self::ProcessedInput) -> String {
-        (0..bricks)
-            .map(|i| count_dropped(i, &resters, &supporters))
-            .sum::<usize>()
-            .to_string()
+    fn solve_part2(counts: Self::ProcessedInput) -> String {
+        counts.into_iter().sum::<usize>().to_string()
     }
 }
 
